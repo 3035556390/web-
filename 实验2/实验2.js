5f8887d2154665302e4ffd51dd6dc4acf5f7326f
莱万汀 <15851770+zhouzeyong1@user.noreply.gitee.com>
2025-09-28 14:08:27 +0800

    提交说明

---
+++ 实验2/实验2.js
@@ -0,0 +1,39 @@
+const elements = document.getElementsByTagName("dd");
+
+Array.from(elements).forEach(element => {
+    element.addEventListener("click", function () {
+        Array.from(elements).forEach(el => {
+            el.classList.remove("active");
+        })
+        element.classList.add("active");
+    })
+})
+const li = document.querySelectorAll(".daohang>ul>li");
+
+Array.from(li).forEach(element => {
+    element.addEventListener("mouseover", function () {
+        const dl = this.querySelector("dl");
+
+        if (dl) { dl.style.maxHeight = "500px" }
+        const dd = dl.querySelectorAll("dd");
+        dd.forEach((el, index) => {
+            el.style.opacity = "1"
+            el.style.transitionDelay = `${0.1 * (index + 1)}ms`;
+
+        })
+
+
+    })
+    element.addEventListener("mouseout", function () {
+        const dl = this.querySelector("dl");
+        if (dl) { dl.style.maxHeight = "0" }
+        const dd = dl.querySelectorAll("dd");
+        dd.forEach(el => {
+            el.style.opacity = "0"
+            el.style.transitionDelay = "0s"
+        })
+    })
+
+
+})
+
